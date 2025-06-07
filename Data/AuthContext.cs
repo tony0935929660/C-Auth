5a7cd5b86a071eeac96934a05454aa604f036e7e
jflin <tony0970941055@gmail.com>
2025-06-07 16:52:47 +0800

    feat: sync database by entityframeworkCore

---
+++ Data/AuthContext.cs
@@ -0,0 +1,13 @@
+﻿using Auth.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Data
+{
+    public class AuthContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=localhost;Database=Auth;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,7 @@
 {
     public class User
     {
+        public int Id { get; set; }
         public required string Username { get; set; }
         public required string Password { get; set; }
         public required string Role { get; set; }
